d6061132242809b139230e92325675a55fe12a1b
Shadowlized <61539939+Shadowlized@users.noreply.github.com>
2020-06-08 20:40:34 +0800

    Add files via upload

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'login');
+define('DBUSER', 'root');
+define('DBPASS', 'YWu43382');
+define('DBCONNSTRING','mysql:host=localhost;dbname=login');
+?>
